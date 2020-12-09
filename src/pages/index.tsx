498b87650dac600a1f17d386726dee135796c284
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2020-12-09 00:02:14 -0300

    refactor: rename files as ts extensions

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <main className="">
+      <h1 className="mb-20 flex pt-16">
+        Welcome to Home <a href="https://nextjs.org">Next.js!</a>
+      </h1>
+    </main>
+  )
+}
+
+export default Home
