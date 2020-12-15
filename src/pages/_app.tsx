96f7921f8d396830a6157a77542886e52d111604
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2020-12-15 00:03:37 -0300

    chore: connect amplify to next app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,13 @@
 import { AppProps } from 'next/app'
 
+import Amplify from 'aws-amplify'
+import config from '../../aws-exports'
+
+Amplify.configure({
+  ...config,
+  ssr: true,
+})
+
 import 'styles/main.css'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
